1b11a02295d1f6b34231081036616bc4a8f45987
Muhammed Rabeeh <rabeehm1999@gmail.com>
2025-02-16 19:22:08 +0530

    fix(asgi): ensure django setup before importing routes

--- backend/asgi.py
+++ backend/asgi.py
@@ -8,15 +8,18 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from api.route import websocket_urlpatterns
 
 settings_module = 'backend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backend.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
+django.setup()
+
+from api.route import websocket_urlpatterns
 application = get_asgi_application()
 
 application = ProtocolTypeRouter({
