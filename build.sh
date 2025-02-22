e48725007c50b352e3fbfa4313f5677a12a7e4d9
Muhammed Rabeeh <rabeehm1999@gmail.com>
2025-02-23 00:12:16 +0530

    fix: set superuser for deployment

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-if [[$CREATE_SUPERUSER]];
-then
+if [[ $CREATE_SUPERUSER == "True" ]]; then
     python manage.py createsuperuser --no-input
 fi
